e665565c982b64ea1d1b661df1c2acb90156c77c
Oleg Krasavin <ifrexd@gmail.com>
2024-01-20 18:24:07 +0100

    first

---
+++ README.md
@@ -0,0 +1,20 @@
+# Voice2Text CLI Utility
+
+This is a simple command-line utility for converting voice to text. It uses the `openai/whisper-large-v3` model from Hugging Face's `transformers` library for automatic speech recognition.
+
+## Installation
+
+To install the necessary dependencies, run the following command:
+
+```bash
+pip install -r requirements.txr
+```
+
+## Usage
+You can run the script with the path to a .wav file as a command-line argument. For example:
+
+```bash
+python app.py path_to_your_file.wav
+```
+
+The script will print the transcription of the audio file to the console.
---
+++ app.py
@@ -0,0 +1,40 @@
+import argparse
+import torch
+from transformers import pipeline
+from transformers.pipelines.audio_utils import ffmpeg_read
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Transcribe a wav file.')
+parser.add_argument('wavfile', type=str, help='The path to the wav file to transcribe.')
+args = parser.parse_args()
+
+MODEL_NAME = "openai/whisper-large-v3"
+BATCH_SIZE = 8
+FILE_LIMIT_MB = 1000
+YT_LENGTH_LIMIT_S = 3600  # limit to 1 hour YouTube files
+
+device = 0 if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+
+print(f"Using device: {device}")
+
+pipe = pipeline(
+    task="automatic-speech-recognition",
+    model=MODEL_NAME,
+    chunk_length_s=30,
+    device=device,
+)
+
+def transcribe(inputs, task):
+    if inputs is None:
+        raise Exception("No audio file submitted! Please upload or record an audio file before submitting your request.")
+
+    text = pipe(inputs, batch_size=BATCH_SIZE, generate_kwargs={"task": task}, return_timestamps=True)["text"]
+    return  text
+
+inputs = None
+with open(args.wavfile, "rb") as f:
+    inputs = f.read()
+    inputs = ffmpeg_read(inputs, pipe.feature_extractor.sampling_rate)
+    inputs = {"array": inputs, "sampling_rate": pipe.feature_extractor.sampling_rate}
+    text = pipe(inputs, batch_size=BATCH_SIZE, generate_kwargs={"task": "transcribe"}, return_timestamps=True)["text"]
+    print(text)
